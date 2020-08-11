c6d11f1d64bc92cede94111e04164982fe2c5ab2
Sergio Rodríguez <sergio_eliot@hotmail.com>
2020-08-11 15:48:36 -0600

    Changed solution location so hopefully it's picked up by the tracis yml config.
    Signed-off-by: Sergio Rodríguez <sergio_eliot@hotmail.com>

--- pipedream-travis-ci-trigger-build/Program.cs
+++ pipedream-travis-ci-trigger-build/Program.cs
@@ -7,7 +7,7 @@ namespace pipedream_travis_ci_trigger_build
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("Seeing if this change triggers a build!");
+            Console.WriteLine("BUilt by travis");
             ;
         }
     }
