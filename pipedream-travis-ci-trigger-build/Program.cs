63a4f16bb002830301ea7a51ca4c8cb5fb75c1fe
Sergio Rodríguez <sergio_eliot@hotmail.com>
2020-08-11 14:51:12 -0600

    Added a c# project in order to be build from travis ci, triggered from pipedream
    Signed-off-by: Sergio Rodríguez <sergio_eliot@hotmail.com>

---
+++ pipedream-travis-ci-trigger-build/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace pipedream_travis_ci_trigger_build
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            ;
+        }
+    }
+}
